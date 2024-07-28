4752618f032f4358145bf6045e32f53edf9f66ac
nakkinn <81859514+nakkinn@users.noreply.github.com>
2024-07-28 20:25:45 +0900

    Delete soukantai directory

--- soukantai/common_script2.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("mySelect").addEventListener('change',()=>{
-    var selectedValue = document.getElementById("mySelect").value;
-    window.location.href = selectedValue;
-});
